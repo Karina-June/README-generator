d003e9d7c06be2f4d96e2b73fa6a413578ea4d21
Karina June <116246298+KarinaJune@users.noreply.github.com>
2023-02-05 22:03:38 -0600

    created function and array for questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,46 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =   [
+{
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your project?',
+},
+{
+    type: 'checkbox',
+    message: 'Added sections?',
+    name: 'sections',
+    choices: ['Description', 'Installation', 'Usage', 'Contributing', 'Tests'],
+},
+{
+    type: 'list',
+    message: 'what is the license for your application?',
+    name: 'license',
+    choices: ['None', 'MIT', 'GNU', 'Mozilla Public'],
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'Please enter GitHub username.'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Please enter email address.'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    {
+    fs (fileName, data);
+    console.log(`wrote data to ${filename}`);
+     } (error) {
+        console.log(`got an error ${error.message}`)
+    }
+}
 
 // TODO: Create a function to initialize app
 function init() {}
